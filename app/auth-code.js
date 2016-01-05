685d825a0d603a8ba38e63d35d279b8440565250
Shawn Lim <shawnlim.ws@gmail.com>
2016-01-05 11:40:53 +0800

    added notifications

---
+++ app/auth-code.js
@@ -0,0 +1,8 @@
+curl --header "Authorization: key=AIzaSyCmX8OsnHG8uXJyhoNaI9rtLkf4myZsHRY" \
+       --header Content-Type:"application/json" \
+       https://gcm-http.googleapis.com/gcm/send \
+       -d "{\"registration_ids\":[\"f6z5KH_wAEM:APA91bGr6p5eTt8TxkeUy3Z2xRRixRV2UguMZ2JYezVr__9Dm9Y_Ev8eFIssg-HLN_BBPCIMg3bgW9p1gRUeMHeyFolAekj81XPXJhPW3Q_pmWJ8HR2ycCUWaK2mdymHui2kRSzargeJ\"]}"
+
+       https://android.googleapis.com/gcm/send/f6z5KH_wAEM:APA91bGr6p5eTt8TxkeUy3Z2xRRixRV2UguMZ2JYezVr__9Dm9Y_Ev8eFIssg-HLN_BBPCIMg3bgW9p1gRUeMHeyFolAekj81XPXJhPW3Q_pmWJ8HR2ycCUWaK2mdymHui2kRSzargeJ
+
+https://android.googleapis.com/gcm/send/f6z5KH_wAEM:APA91bGr6p5eTt8TxkeUy3Z2xRRixRV2UguMZ2JYezVr__9Dm9Y_Ev8eFIssg-HLN_BBPCIMg3bgW9p1gRUeMHeyFolAekj81XPXJhPW3Q_pmWJ8HR2ycCUWaK2mdymHui2kRSzargeJ
--- app/sw.js
+++ app/sw.js
@@ -19,29 +19,24 @@
 
 // Version 0.1
 
-'use strict';
+'use strict'
 
-console.log('Started', self);
-
-self.addEventListener('install', function(event) {
-  self.skipWaiting();
-  console.log('Installed', event);
-});
-
-self.addEventListener('activate', function(event) {
-  console.log('Activated', event);
-});
-
-self.addEventListener('push', function(event) {
-  console.log('Push message', event);
-
-  var title = 'Push message';
-
+console.log('Started', self)
+self.addEventListener('install', function (event) {
+  self.skipWaiting()
+  console.log('Installed', event)
+})
+self.addEventListener('activate', function (event) {
+  console.log('Activated', event)
+})
+self.addEventListener('push', function (event) {
+  console.log('Push message', event)
+  var title = 'Push message'
   event.waitUntil(
     self.registration.showNotification(title, {
-      'body': 'The Message',
-      'icon': 'images/icon.png'
-    }));
-});
-
+      body: 'Whats up buttercub!',
+      icon: 'images/icon.png',
+      tag: 'my-tag'
+    }))
+})
 // TODO
